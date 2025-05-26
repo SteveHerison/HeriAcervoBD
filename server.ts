a2b7199922e83af078731a81e4a4374e5d1c4291
steveherison <steveherisondev@gmail.com>
2025-05-26 18:44:50 -0300

    Corrigido build do TypeScript

--- server.ts
+++ server.ts
@@ -8,30 +8,36 @@ import path from "path"; // ✅ Adicione isso
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:3000", // para desenvolvimento local
-  "https://acervoocupacional.vercel.app", // seu frontend principal em produção
-  "https://heri-acervo.vercel.app", // outro domínio do frontend que você mencionou
-];
+// const allowedOrigins = [
+//   "http://localhost:3000", // para desenvolvimento local
+//   "https://acervoocupacional.vercel.app", // principal em produção
+//   "https://heri-acervo.vercel.app", // outro domínio
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true); // permite requisições sem origin (ex: Postman)
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg = `O CORS para a origem ${origin} não é permitido.`;
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // permite requisições sem origin (ex: Postman)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `O CORS para a origem ${origin} não é permitido.`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
+    origin: "*",
   })
 );
 
 app.use(express.json());
 
 // ✅ Aqui você adiciona o middleware para servir as imagens
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // ✅ Depois disso, registre as rotas da aplicação
 app.use("/", router);
